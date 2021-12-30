919e806f175d02ff5d416c59bbf1be1d671308d1
lvseouren <602633512@qq.com>
2021-12-30 17:31:56 +0800

    init

---
+++ Scripts/RunTime/PgLocalizeUtil.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine.Localization.Settings;
+
+public static class PgLocalizeUtil
+{
+    public static string GetLocalizeString(string collection, string key)
+    {
+        var table = LocalizationSettings.StringDatabase.GetTable(collection);
+        return table.GetEntry(key)?.LocalizedValue;
+    }
+
+    public static string GetLocalizeString(string key)
+    {
+        var data = key.Split('_');
+        return GetLocalizeString(data[0], key);
+    }
+}
+
